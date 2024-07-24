8a4747562a78f0ed468b6cb70a0778190b9b9b84
Rolce5 <your.titashipuserge@gmail.com>
2024-07-24 09:34:12 +0100

    first commit

---
+++ views/layouts/header.php
@@ -0,0 +1,12 @@
+<div class="header-wrapper">
+    <div class="header-content">
+        <img src="" alt="">
+    </div>
+    <nav class="header-nav">
+        <ul class="nav-links">
+            <li class="nav-items">
+                <a href="" class="nav-link">Home</a>
+            </li>
+        </ul>
+    </nav>
+</div>
