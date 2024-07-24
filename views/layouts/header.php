72e22fe50b6e90ec767e4b94245f02c18fb62dae
Rolce5 <your.titashipuserge@gmail.com>
2024-07-24 11:09:56 +0100

    partial header and footer

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -1,12 +1,33 @@
-<div class="header-wrapper">
-    <div class="header-content">
-        <img src="" alt="">
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="/ODC/BIBLIOTECHA/public/assests/css/styles.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="header-wrapper">
+        <div class="header-content">
+            <a class="logo-wrapper" href="">
+                <img src="/ODC/BIBLIOTECHA/public/assests/images/IMG_2771.JPG" alt="">
+                <!-- <img src="/public/assests/images/IMG_2771.JPG" alt=""> -->
+            </a>
+            <nav class="header-nav">
+                <ul class="nav-links">
+                    <li class="nav-items">
+                        <a href="" class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-items">
+                        <a href="" class="nav-link">Catalogue</a>
+                    </li>
+                    <li class="nav-items">
+                        <a href="" class="nav-link">Login</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </div>
-    <nav class="header-nav">
-        <ul class="nav-links">
-            <li class="nav-items">
-                <a href="" class="nav-link">Home</a>
-            </li>
-        </ul>
-    </nav>
-</div>
