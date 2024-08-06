a933e04262766cd2834bd55ca61113ecf26cc472
Smak-tech <mbuagbosmak@gmail.com>
2024-08-06 15:37:14 +0100

    contact page done

---
+++ views/contact.php
@@ -0,0 +1,10 @@
+<?php
+    include_once '../views/layouts/header.php'
+ ?>
+<div>
+<button class="btn btn-primary">rew</button>
+</div>
+
+<?php
+    include_once '../views/layouts/footer.php'
+ ?>
